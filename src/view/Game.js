603e99693ac8e61ebb1f6fab7d2d01fdf554b49d
lugomiguel <lugozamoraiw@gmail.com>
2022-05-02 17:12:29 -0500

    CartasyFlexboxAgregados

--- src/view/Game.js
+++ src/view/Game.js
@@ -1,21 +1,32 @@
 import React from "react";
 import img6 from "../img/img_6.jpeg"
 import {Link} from 'react-router-dom'
+import avatar from "../img/avatar.png"
 
 function Lobby() {
     return (
-        <div>
-          <div>
-            <Link to="/lobby">Abandonar</Link>           
+        <div style = {{
+          display:"flex",
+          }} >
+          <div style = {{
+                        width: "68%",
+                        backgroundColor:"black"}}> 
+            <img src={avatar} />        
           </div>
-          <div>
-            
+          <div  style = {{
+                        width: "32%",
+                        backgroundColor:"pink"}}>
+            <Link to="/lobby" style = {{border: '5px solid #555', 
+                      height: "12vh",
+                      marginLeft: "2px",
+                      marginRight: "20vw"}}>Abandonar</Link>
+            <img 
+            style = {{border: '5px solid #555', 
+                      height: "12vh",
+                      right: ""
+                      }}
+            src={avatar}/>            
           </div>
-        <div>
-          <h1>La ultima vista es el juego</h1>
-        </div>
-        <br/>
-        <img src={img6}/>
       </div> 
     );
   }
